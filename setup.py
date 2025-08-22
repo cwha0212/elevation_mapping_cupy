7661ee891130776add73a8c0e3823a12ad517844
JonasFrey96 <jonfrey@ethz.ch>
2025-08-22 14:24:04 +0200

    added all

--- setup.py
+++ setup.py
@@ -24,9 +24,7 @@ setup(
     include_package_data=True,
     package_data={
         "elevation_mapping_cupy": [
-            "config/*",
-            "*.yaml",
-            "*.yml",
+            "*",
         ],
     },
 )
